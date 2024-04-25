9cc6b5156771c182d0e3b6b5b68d7fc92ef364cf
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-04-26 00:16:06 +0200

    fix: date formatting and being able to choose future date

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import type { SearchQuery } from "../types";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 interface DatePickerProps {
   searchQuery: SearchQuery;
@@ -8,19 +10,28 @@ interface DatePickerProps {
 
 export function DatePicker({ searchQuery }: DatePickerProps) {
   const [dayElements, setDayElements] = useState<JSX.Element[]>([]);
+  const [currentStartDate, setCurrentStartDate] = useState<Date>(new Date(Date.parse(searchQuery.startDate)));
 
   function millisecondsToWholeDays(milliseconds: number): number {
     return Math.round(Math.floor(milliseconds / 1_000 / 60 / 60 / 24));
   }
 
-  function drawCalendar(daysDifference: number, startDate: Date, endDate: Date): JSX.Element[] {
-    const daysBetween: Date[] = [];
+  function drawCalendar(startDate: Date, daysDifference?: number): JSX.Element[] {
+    let daysBetween: Date[] = [];
     const dayElements: JSX.Element[] = [];
+    const today = new Date(Date.now()).toLocaleDateString();
 
     for (let i = 1; i <= 35; i++) {
       const dayBetween = new Date(startDate);
       dayBetween.setDate(dayBetween.getDate() + i);
       daysBetween.push(dayBetween);
+    }
+    
+    for (let i = 0; i < daysBetween.length; i++) {
+      if (daysBetween[i].toLocaleDateString() === today) {
+        daysBetween = daysBetween.slice(0, i);
+        break;
+      }
     }
 
     for (let i = 0; i < daysBetween.length; i++) {
@@ -28,8 +39,8 @@ export function DatePicker({ searchQuery }: DatePickerProps) {
         <button
           type="button"
           key={i}
-          className={i <= daysDifference ? "border-r-2 border-b-2 bg-lime-900" : "border-r-2 border-b-2"}>
-          {daysBetween[i].getDate()}
+          className={daysDifference && i <= daysDifference ? "border-r-2 border-b-2 bg-lime-900" : "border-r-2 border-b-2"}>
+          {daysBetween[i].getDate() + "/" + (daysBetween[i].getMonth() + 1)}
         </button>
       );
       dayElements.push(dayElement);
@@ -38,16 +49,31 @@ export function DatePicker({ searchQuery }: DatePickerProps) {
     return dayElements;
   }
 
+  function handleClick(isBack: boolean) {
+    const currentStartDateCopy = new Date(currentStartDate);
+
+    if (isBack) {
+      currentStartDate.setDate(currentStartDate.getDate() - 35);
+    } else {
+      currentStartDate.setDate(currentStartDate.getDate() + 35);
+    }
+    setCurrentStartDate(currentStartDateCopy)
+    console.log(currentStartDate)
+    const days: JSX.Element[] = drawCalendar(currentStartDate);
+    setDayElements([...days]);
+  }
+
   useEffect(() => {
     const millisecondsStartDate = Date.parse(searchQuery.startDate);
     const dateStartDate = new Date(Date.parse(searchQuery.startDate));
 
     const millisecondsEndDate = Date.parse(searchQuery.endDate);
     const dateEndDate = new Date(Date.parse(searchQuery.endDate));
+    console.log(dateEndDate)
 
     const daysDifference = millisecondsToWholeDays(millisecondsEndDate - millisecondsStartDate);
 
-    const days: JSX.Element[] = drawCalendar(daysDifference, dateStartDate, dateEndDate);
+    const days: JSX.Element[] = drawCalendar(dateStartDate, daysDifference);
     setDayElements([...days]);
   }, [searchQuery.startDate, searchQuery.endDate]);
 
@@ -64,7 +90,8 @@ export function DatePicker({ searchQuery }: DatePickerProps) {
       </div>
       <div className="days-container grid grid-cols-7 grid-rows-5 *:p-2">{dayElements}</div>
       <div>
-        
+        <button type="button" onClick={() => handleClick(true)}><ArrowBackIcon/></button>
+        <button type="button" onClick={() => handleClick(false)}><ArrowForwardIcon /> </button>
       </div>
     </div>
   );
